76aecc494ed45d3a32f16ab3dfae545cd2fc0167
patricia-arisan <137221366+patricia-arisan@users.noreply.github.com>
2026-02-13 22:00:26 +0100

    Cambios header user

--- frontend/src/app/pages/shared/headers/header-user/header-user.component.ts
+++ frontend/src/app/pages/shared/headers/header-user/header-user.component.ts
@@ -3,7 +3,9 @@ import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../../../services/user-service';
 import { Usuario } from '../../../../interfaces/usuario';
 
-
+/**
+ * Componente para recuperar la informacion del usuario para la navegacion
+ */
 @Component({
   selector: 'app-header-user',
   standalone: true,
@@ -11,84 +13,62 @@ import { Usuario } from '../../../../interfaces/usuario';
   templateUrl: './header-user.component.html',
   styleUrl: './header-user.component.css'
 })
-export class HeaderUserComponent implements OnInit{
+export class HeaderUserComponent implements OnInit {
   user!: Usuario;
-  showMenu: Boolean = true;
-  showAccount: Boolean = true;
-  
+  hideMenu: Boolean = true;
+  hideAccount: Boolean = true;
 
   constructor(
     private router: Router,
     private userService: UserService,
     private eRef: ElementRef
-    
-  ){}
+  ) {}
 
+  // Recuperacion del usuario al iniciar el componente
   ngOnInit(): void {
-
     this.retrieveFromLocalStorage();
   }
 
-retrieveFromLocalStorage() {
-
-    this.user = JSON.parse(localStorage.getItem('usuario') || '')
-    //this.user = JSON.parse(localStorage.getItem('usuario') || '')
+  // Funcion para recuperar al usuario y cargar sus datos en el formulario
+  retrieveFromLocalStorage() {
+    // Recuperacion del usuario actual mediante el id guardado en el localstorage
     let value = this.userService.getItem('id');
-    //let credentials = localStorage.getItem('token') || ''
-    //   let credentials = localStorage.getItem('token') || ''
-    //let cred = JSON.stringify(credentials);
-    console.log("Value antes de current: " + value)
-
     let currentUser = 0;
-    if (value !== null) { //en angular ===
+    if (value !== null) {
       currentUser = parseInt(value);
-      console.log("STRING A INT" + currentUser);
-
-      //this.userService.getLoggedUser(currentUser,this.user).subscribe((data:Usuario)=>{
-
       this.userService.getLoggedUser(currentUser).subscribe((data: Usuario) => {
-
         this.user = data;
-        console.log("DATOS HOME " + this.user);
-        console.log("Local" + this.user.id);
-
       });
-
     }
   }
-  logout(){
-    this.userService.logoutUser().subscribe(()=>{
+
+  // Funcion para cerrar sesion y limpiar los datos del usuario
+  logout() {
+    this.userService.logoutUser().subscribe(() => {
       localStorage.clear();
       this.router.navigate(['/login']);
     });
-    
   }
 
-  toggleView(){ 
-    this.showMenu = !this.showMenu;
-    
-    
-    
-    
+  // Funcion para mostrar u ocultar el menu desplegable
+  toggleViewMenu() {
+    this.hideMenu = !this.hideMenu;
   }
 
-  toggleViewAccount(){ 
-    this.showAccount = !this.showAccount;
-    
-    
-    
-    
+  // Funcion para mostrar u ocultar las opciones del usuario
+  toggleViewAccount() {
+    this.hideAccount = !this.hideAccount;
   }
 
   @HostListener('document:click', ['$event'])
+  // Funcion para cerrar los desplegables al pinchar por la pantalla
   clickOut(event: Event) {
-    // Cerrar deplegable al pinchar fuera de la lista
     if (!this.eRef.nativeElement.contains(event.target)) {
-      this.showAccount = true;
+      this.hideAccount = true;
     }
 
     if (!this.eRef.nativeElement.contains(event.target)) {
-      this.showMenu = true;
+      this.hideMenu = true;
     }
   }
 }
