1812abe6b8cde8476393704a54da6ea8238ad037
patricia-arisan <137221366+patricia-arisan@users.noreply.github.com>
2026-03-16 20:11:46 +0100

    Javadoc httpsession y comentaros application

--- goBooks/src/main/java/com/atrium/gobooks/configuration/HttpSessionConfig.java
+++ goBooks/src/main/java/com/atrium/gobooks/configuration/HttpSessionConfig.java
@@ -5,17 +5,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+/**
+ * Configuracion de Spring Session orientada al uso de Redis como almacenamiento.
+ * Gestiona la serializacion de datos y la interaccion con el servidor
+ */
 @Configuration
-////@EnableRedisHttpSession
 public class HttpSessionConfig {
+	/**
+	 * Deshabilita la configuracion automatica, no ejecuta el comando CONFIG en Redis 
+	 * @return una accion NO_OP que no realiza acciones de configuracion en Redis
+	 */
 	@Bean
 	public static ConfigureRedisAction configureRedisAction() {
 		return ConfigureRedisAction.NO_OP;
 	}
-	 @Bean
-	    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
-	        return new GenericJackson2JsonRedisSerializer();
-	    }
+
+	/**
+	 * Define el serializador por defecto para los datos de sesion de Redis.
+	 * Se utiliza json en vez de la serializacion nativa de Java para que sea mas legible
+	 * @return el serializador de objetos basado en json
+	 */
+	@Bean
+	public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+		return new GenericJackson2JsonRedisSerializer();
+	}
 }
